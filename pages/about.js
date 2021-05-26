ecec78513a98eabab1ce597fd500f10e4dadbc32
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-05-26 18:54:49 +0100

    Add logo image to replade initial h1 tag

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,26 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Nulla pariatur est in tempor elit sint quis labore fugiat voluptate ea
-        dolor laborum magna. Reprehenderit eiusmod culpa fugiat irure magna
-        ipsum sint culpa.
-      </p>
-      <p>
-        Nulla pariatur est in tempor elit sint quis labore fugiat voluptate ea
-        dolor laborum magna. Reprehenderit eiusmod culpa fugiat irure magna
-        ipsum sint culpa.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Nulla pariatur est in tempor elit sint quis labore fugiat voluptate ea
+          dolor laborum magna. Reprehenderit eiusmod culpa fugiat irure magna
+          ipsum sint culpa.
+        </p>
+        <p>
+          Nulla pariatur est in tempor elit sint quis labore fugiat voluptate ea
+          dolor laborum magna. Reprehenderit eiusmod culpa fugiat irure magna
+          ipsum sint culpa.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,27 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Ullamco officia cillum reprehenderit minim. Ullamco ad elit ad aliqua ex
-        excepteur enim et aliqua do est. Proident ad elit quis enim do mollit
-        minim pariatur.
-      </p>
-      <p className={styles.text}>
-        Ullamco officia cillum reprehenderit minim. Ullamco ad elit ad aliqua ex
-        excepteur enim et aliqua do est. Proident ad elit quis enim do mollit
-        minim pariatur.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Ullamco officia cillum reprehenderit minim. Ullamco ad elit ad aliqua
+          ex excepteur enim et aliqua do est. Proident ad elit quis enim do
+          mollit minim pariatur.
+        </p>
+        <p className={styles.text}>
+          Ullamco officia cillum reprehenderit minim. Ullamco ad elit ad aliqua
+          ex excepteur enim et aliqua do est. Proident ad elit quis enim do
+          mollit minim pariatur.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
