996d618444a84ff9945fc744b384385cb89e7cd8
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-05-26 18:25:57 +0100

    Add Link component to pages/index.js & navbar.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Navbar from "../comps/Navbar";
 import Footer from "../comps/Footer";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -19,6 +20,9 @@ export default function Home() {
         excepteur enim et aliqua do est. Proident ad elit quis enim do mollit
         minim pariatur.
       </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );
