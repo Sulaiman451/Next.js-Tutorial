e58228dc27f039c371710d6abc1caeec89132e94
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-05-26 18:31:24 +0100

    Create a layout component to wrap page content

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Ullamco officia cillum reprehenderit minim. Ullamco ad elit ad aliqua ex
@@ -23,7 +22,6 @@ export default function Home() {
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
