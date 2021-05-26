abc3fa8d0ef8a04707c57aa3896032eb1ed97c18
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-05-26 18:18:51 +0100

    Create footer & navbar components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../comps/Navbar";
+import Footer from "../comps/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Ullamco officia cillum reprehenderit minim. Ullamco ad elit ad aliqua ex
@@ -16,6 +19,7 @@ export default function Home() {
         excepteur enim et aliqua do est. Proident ad elit quis enim do mollit
         minim pariatur.
       </p>
+      <Footer />
     </div>
   );
 }
